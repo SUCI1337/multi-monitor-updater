62eb8e76059aa19409a107260c9b9f4ec918af3d
Michal.Swiecicki <22434990+mswiecic@users.noreply.github.com>
2023-04-21 14:28:17 +0200

    Version update

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ const config: CliOptions = {
   environmentUrl: 'https://umsaywsjuo.dev.apps.dynatracelabs.com/',
   app: {
     name: 'Multi-Monitor Updater',
-    version: '1.1.0',
+    version: '1.1.1',
     description: 'Verify and update your Synthetic monitor configurations',
     id: 'my.multi.monitor.updater',
     scopes: [
