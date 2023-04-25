3bf3b3f36c9a1097faf46344059074a5fb02ad25
mswiecic <22434990+mswiecic@users.noreply.github.com>
2023-04-25 11:12:06 +0200

    Removing internals urls (#12)
    * Update package.json and package-lock.json
    
    * Update package.json and package-lock.json

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,10 @@
-import type { CliOptions } from '@dynatrace/dt-app';
+import type { CliOptions } from 'dt-app';
 
 const config: CliOptions = {
   environmentUrl: 'https://umsaywsjuo.dev.apps.dynatracelabs.com/',
   app: {
     name: 'Multi-Monitor Updater',
-    version: '1.1.1',
+    version: '1.1.2',
     description: 'Verify and update your Synthetic monitor configurations',
     id: 'my.multi.monitor.updater',
     scopes: [
--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,4 +1,3 @@
 export const wildcard = '*';
 export const empty = '';
 export const keyValueSeparator = '#&@#';
-export const defaultPreviewHeightFactor = 38;
