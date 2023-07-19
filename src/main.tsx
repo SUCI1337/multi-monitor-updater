f16b6e04baa5d825808b38ff82bf5e9d99c48e01
Lopez, Manu <manu.lopez@dynatrace.com>
2023-07-19 16:33:31 +0200

    Pull request #17: feature/SR-2621 Add page view tracking
    Merge in APPS/multi-monitor-updater from feature/SR-2621-multi-monitor-updater-instrument-page-view-events to main
    
    * commit '9b3e64253ac607146b5e84bba71041894a7e567a':
      feature/SR-2621 Add page view tracking
    
    GitOrigin-RevId: 98677a51ec20398550a425b5426541fdc504b6c7

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppRoot } from '@dynatrace/strato-components-preview';
-import { BrowserRouter } from 'react-router-dom';
+import { createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 import { App } from './app/App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Tracking } from '@dynatrace-sdk/ui-monitoring-events';
+import { createBrowserRouter } from '@dynatrace-sdk/ui-monitoring-events/react-router-dom-v6';
 
 // Create a client
 const queryClient = new QueryClient({
@@ -18,14 +20,26 @@ const queryClient = new QueryClient({
   },
 });
 
+const Routing = (
+  <>
+    <Route
+      path=''
+      element={<App />}>
+    </Route>
+  </>
+)
+
 ReactDOM.render(
   <AppRoot>
-    <QueryClientProvider client={queryClient}>
-    <BrowserRouter basename='ui'>
-      <App />
-    </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <Tracking>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider
+          router={createBrowserRouter(createRoutesFromElements(Routing), {
+            basename: '/ui',
+        })}></RouterProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </Tracking>
   </AppRoot>,
   document.getElementById('root'),
 );
